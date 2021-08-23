79ad818ad18982fa1873e386bbcc5e94b6c62f66
shifair <farisnura@gmail.com>
2021-08-24 00:48:03 +0700

    Adding new requests for form

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 require("dotenv").config();
-const express = require("express");
+var { response } = require("express");
+var express = require("express");
 
-const hostname = "127.0.0.1";
-const port = process.env.PORT || 3000;
+var hostname = "127.0.0.1";
+var port = process.env.PORT || 3000;
 
 var app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello GET')
+app.use(express.static('public'))
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + "/" + "index.html")
+})
+
+app.get('/getProcess', (req, res) => {
+    response = {
+        firstName: req.query.firstName,
+        lastName: req.query.lastName
+    }
+
+    console.log(response)
+    res.end(JSON.stringify(response))
+})
+
+app.post('/', (req, res) => {
+    console.log("This is a post request")
+    res.send('Hello Post')
+})
+
+app.delete('/deleteUser', (req, res) => {
+    console.log("This is a delete request")
+    res.send('Hello delete')
+})
+
+app.get('/userList', (req, res) => {
+    console.log('Got a get request')
+    res.send('Page Pattern Match')
 })
 
 var server = app.listen(port, () => {
