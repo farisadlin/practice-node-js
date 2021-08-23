11455b8076666c1a377b519ace287a37128b9415
shifair <farisnura@gmail.com>
2021-08-24 00:23:15 +0700

    Setup express

--- server.js
+++ server.js
@@ -1,40 +1,18 @@
 require("dotenv").config();
-const http = require("http");
-var fs = require("fs");
-var url = require("url");
-// const express = require("express");
-// const { response } = require("express");
+const express = require("express");
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
 
-http
-  .createServer((req, res) => {
-    var pathname = url.parse(req.url).pathname;
+var app = express()
 
-    console.log(`Request for ${pathname} received.`);
+app.get('/', (req, res) => {
+    res.send('Hello GET')
+})
 
-    fs.readFile(pathname.substr(1), (err, data) => {
-      if (err) {
-        console.log(err);
+var server = app.listen(port, () => {
+    var host = server.address().address
+    var port = server.address().port
 
-        // HTTP Status: 404 : NOT FOUND
-        // Content Type: text/plain
-
-        res.writeHead(404, { "Content-Type": "text/html" });
-      } else {
-        //Page found
-        // HTTP Status: 200 : OK
-        // Content Type: text/plain
-        res.writeHead(200, { "Content-Type": "text/html" });
-
-        res.write(data.toString());
-
-        // Write the content of the file to res body
-      }
-
-      res.end();
-    });
-  }).listen(port);
-
-console.log(`Server running at http://127.0.0.1:${port}/`);
+    console.log('Example App listening to http://%s%s', host, port)
+})
