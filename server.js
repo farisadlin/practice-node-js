59219bcf7acddb59e96eab00a11c07203c04bbce
shifair <farisnura@gmail.com>
2021-08-30 23:30:34 +0700

    URL SHORTENER

--- server.js
+++ server.js
@@ -1,54 +1,36 @@
 require("dotenv").config();
-var { response } = require("express");
-var express = require("express");
-var fs = require("fs");
-var multer = require("multer");
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrls");
+const app = express();
 
-var port = process.env.PORT || 3000;
-
-var app = express();
-
-app.use(express.static("public"));
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-
-var multer = require("multer");
-multer({ dest: "/tmp/" });
-
-app.get("/index.htm", function (req, res) {
-  res.sendFile(__dirname + "/" + "index.htm");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.post("/fileUpload", (req, res) => {
-  console.log(req.files.file.name);
-  console.log(req.files.file.path);
-  console.log(req.files.file.type);
-  var file = __dirname + "/" + req.files.file.name;
+app.set("view engine", "ejs");
 
-  fs.readFile(req.files.file.path, function (err, data) {
-    fs.writeFile(file, data, function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        response = {
-          message: "File uploaded successfully",
-          filename: req.files.file.name,
-        };
-      }
+app.use(express.urlencoded({ extended: false }));
 
-      console.log(response);
-      res.end(JSON.stringify(response));
-    });
-  });
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
 });
 
-var server = app.listen(port, () => {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log("Example App listening to http://%s%s", host, port);
+app.post("/api/shortid", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
 });
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl});
+  if (shortUrl === null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+});
+
+app.listen(process.env.PORT || 3000);
